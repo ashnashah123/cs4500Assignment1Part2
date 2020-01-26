bd7ef65258f257a581d86f3b228bbfdcdc401291
dermers <33739058+dermers@users.noreply.github.com>
2020-01-26 17:59:53 -0500

    Create standard for String.h across the spec teams per Piazza post 319
    https://piazza.com/class/k51bluky59n2jr?cid=319

--- string.h
+++ string.h
@@ -1,30 +1,48 @@
-// lang::CwC
+//lang::CwC
+#pragma once
 
-/*
-* This class represents a String. 
-* It contains a method to add characters, grow the string, 
-* and to concat strings together.
-*
-*  author: shah.ash@husky.neu.edu | peters.ci@husky.neu.edu
-*/
+#include "object.h"
+#include <cstdlib>
+#include <cstring>
+#include <cstdio> 
+
+/**
+ * String: a wrapper for char* and constant char* in order to
+ *         allow for easy creating and computation of strings
+ * 
+ * immutable
+ * 
+ * author: dermer.s@husky.neu.edu / dermer.s@northeastern.edu
+ **/
 class String : public Object {
-public: 
-    // This is the list which contains the characters of the string
-    char* val_; 
-    // This is the length of the string 
-    size_t size_;
-    
-    /* Concatenates this String with the given String together and returns the new String
-    * @arg s: the String to concatenate to this String
-    */
-    String* concat(String* s);
-    
-    /* Adds a character to this string
-    * @arg c: the character to be added to this string
-    */
-    void addChar(char* c);
+public:
+    char* val; // owned
 
-    /* Returns the length of this string. 
-    */
-    int length();
+    String(char* c) {
+    }
+
+    String(const char* c) {
+    }
+
+    ~String() { //destructor
+    }
+
+    // hash function based on the equivalence specified by <equals>
+    size_t hash() {
+    }
+
+    // compares strings based on direct character equivalence
+    bool equals(Object *other) {
+    }
+
+    // compares the strings based on alphabetical order
+    virtual int cmp(String *that) {
+    }
+
+    // returns a string made up of the concatination of this string and s
+    String* concat(String *s) {
+    }
+
+    // getter for the size of this string
+    size_t size() {}
 };
